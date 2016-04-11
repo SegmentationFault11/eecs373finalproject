768d8e05fc1dbe246978f478e509935e33cf5d77
Steven Ma <machong@umich.edu>
2016-04-11 19:12:26 -0400

    modified implemetation of steering offset

--- carsoftware/settings.c
+++ carsoftware/settings.c
@@ -6,4 +6,6 @@ void init_settings() {
 	settings.steer_loc = (uint32_t*)(0x40050000);
 	settings.motor_loc = (uint32_t*)(0x40050100);
 	settings.controller_loc = (uint32_t*)(0x40050200);
+
+	settings.steer_offset = 0;
 }
