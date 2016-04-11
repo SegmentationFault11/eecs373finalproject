08a0b9aa0c3ad0e21eac47fde6c9b9c375befa2e
Steven Ma <machong@umich.edu>
2016-04-11 18:58:15 -0400

    removed placeholder code

--- carsoftware/main.c
+++ carsoftware/main.c
@@ -14,8 +14,6 @@ struct Vehicle vehicle;
 
 int main() {
 
-    printf("Program start\r\n");
-
     // Enable necessary drivers
     ACE_init();
     MSS_GPIO_init();
@@ -27,20 +25,8 @@ int main() {
     init_vehicle(1, Script, 1);
 
     volatile unsigned long i = 0;
-    while(1) {
-        set_steer(0);
-        set_motor(0);
-        printf("steer = 0\r\n");
-        for (i = 0; i < 10000000; ++i);
-        set_steer(512);
-        set_motor(200);
-        printf("steer = 512\r\n");
-        for (i = 0; i < 10000000; ++i);
-        set_steer(1023);
-        set_motor(50);
-        printf("steer = 1023\r\n");
-        for (i = 0; i < 10000000; ++i);
-    }
 
+loop:
+    goto loop;
     return 0;
 }
