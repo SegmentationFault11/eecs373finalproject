381cca39aa618d48f4ee6fd165cfa6d26c9a803f
Steven Ma <machong@umich.edu>
2016-04-11 19:11:25 -0400

    modified steering offset

--- carsoftware/settings.h
+++ carsoftware/settings.h
@@ -3,17 +3,14 @@
 #include <inttypes.h>
 
 typedef struct Settings {
-    int emergency_int;
-    int steer_int;
-    int controller_int;
-    int xbee_int;
-    int fire_int;
-    int hit_int;
+	// Memory location of modules
+	uint32_t* steer_loc;
+	uint32_t* motor_loc;
+	uint32_t* controller_loc;
 
-    uint32_t* steer_loc;
-    uint32_t* motor_loc;
-    uint32_t* controller_loc;
-} Settings_t;
+	// Steering offset
+	uint8_t steer_offset;
+} settings_t;
 
 void init_settings();
 
