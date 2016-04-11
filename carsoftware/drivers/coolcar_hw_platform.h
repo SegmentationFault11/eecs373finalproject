b42eb56d5412baaaa02fd7998f18e60efa97a913
Steven Ma <machong@umich.edu>
2016-04-11 19:24:30 -0400

    added cortex m3/smartfusion drivers

---
+++ carsoftware/drivers/coolcar_hw_platform.h
@@ -0,0 +1,19 @@
+#ifndef coolcar_HW_PLATFORM_H_
+#define coolcar_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Microsemi SmartDesign  Tue Apr 05 23:54:13 2016
+*
+*Memory map specification for peripherals in coolcar
+*/
+
+/*-----------------------------------------------------------------------------
+* MSS_CM3_0 subsystem memory map
+* Master(s) for this subsystem: MSS_CM3_0 
+*---------------------------------------------------------------------------*/
+#define STEERING_0                      0x40050000U
+#define REARMOTOR_0                     0x40050100U
+#define PS2CONTROLLER_0                 0x40050200U
+
+
+#endif /* coolcar_HW_PLATFORM_H_*/
