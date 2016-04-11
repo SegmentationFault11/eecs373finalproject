42df6308377719f5f9f5c022a64e61781570dd2f
Steven Ma <machong@umich.edu>
2016-04-11 19:00:37 -0400

    added car performance parameters

---
+++ carsoftware/performance.h
@@ -0,0 +1,68 @@
+#ifndef PERFORMANCE_H_
+#define PERFORMANCE_H_
+
+// General statistics
+#define DEFAULT_LIVES 1
+
+
+// Tank statistics
+#define TANK_BASE_HEALTH 		250
+#define TANK_BASE_POWER 		0.1
+#define TANK_BASE_WEIGHT 		200
+#define TANK_IMPACT_THRESH 		400
+#define TANK_BASE_DAMAGE		100
+
+#define TANK_INCR_POWER			0.05
+#define TANK_MAX_POWER			0.25
+#define TANK_INCR_DAMAGE		50
+#define TANK_MAX_DAMAGE			250
+
+// F1 statistics
+#define F1_BASE_HEALTH 			50
+#define F1_BASE_POWER 			0.8
+#define F1_BASE_WEIGHT 			20
+#define F1_IMPACT_THRESH 		300
+#define F1_BASE_DAMAGE			20
+
+#define F1_INCR_POWER			0.05
+#define F1_MAX_POWER			1.0
+#define F1_INCR_DAMAGE			10
+#define F1_MAX_DAMAGE			50
+
+// Toyota statistics
+#define TOYOTA_BASE_HEALTH 		100
+#define TOYOTA_BASE_POWER 		0.5
+#define TOYOTA_BASE_WEIGHT 		100
+#define TOYOTA_IMPACT_THRESH 	320
+#define TOYOTA_BASE_DAMAGE		50
+
+#define TOYOTA_INCR_POWER		0.05
+#define TOYOTA_MAX_POWER		0.75
+#define TOYOTA_INCR_DAMAGE		10
+#define TOYOTA_MAX_DAMAGE		100
+
+// LOL statistics
+#define LOL_BASE_HEALTH 		10
+#define LOL_BASE_POWER 			0.1
+#define LOL_BASE_WEIGHT 		20
+#define LOL_IMPACT_THRESH 		300
+#define LOL_BASE_DAMAGE			10
+
+#define LOL_INCR_POWER			0.05
+#define LOL_MAX_POWER			1.0
+#define LOL_INCR_DAMAGE			5
+#define LOL_MAX_DAMAGE			255
+
+// Script statistics
+#define SCRIPT_BASE_HEALTH 		255
+#define SCRIPT_BASE_POWER 		1
+#define SCRIPT_BASE_WEIGHT 		255
+#define SCRIPT_IMPACT_THRESH 	1000
+#define SCRIPT_BASE_DAMAGE		255
+
+#define SCRIPT_INCR_POWER		0
+#define SCRIPT_MAX_POWER		1.0
+#define SCRIPT_INCR_DAMAGE		0
+#define SCRIPT_MAX_DAMAGE		255
+
+#endif /* PERFORMANCE_H_ */
